08c6ed8264db391194aea3133e3307573be7325c
connie_sykes <constanza.hidalgo@usm.cl>
2024-11-06 00:53:17 -0300

    redireccion create

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -24,7 +24,7 @@ function Admin() {
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-3'>
-                <Link to='/create' className='btn btn-success'>CREAR +</Link>
+                <Link to='create' className='btn btn-success'>CREAR +</Link>
                 <table className='table'>
                     <thead>
                         <tr>
