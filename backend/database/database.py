42482b01b998ee7ffb90928824afde8432865e02
Mati2F <matias.fernandezf@usm.cl>
2025-06-17 20:09:48 -0400

    database

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,6 @@ from sqlmodel import create_engine, SQLModel, Session, text, SQLModel, Column, I
 from datetime import datetime, date
 
 servername = ".\\SQLEXPRESS"
-#servername = "DESKTOP-4VTMM2I"
 db_name = "grupo07"
 driver = "ODBC+Driver+17+for+SQL+Server"
 
